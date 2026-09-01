8c14fb65337b255fd54f2d234b2e8978469a5c84
Aslin Yeasudas <aslinyesudas@gmail.com>
2026-09-01 23:16:59 +0530

    typecasting in python

---
+++ Typecasting.py
@@ -0,0 +1,24 @@
+#Typecasting = the process of converting a variable from one data type to another
+#              str(), int(), float(), bool()
+
+name="Bro Code"
+age=25
+gpa=3.2
+is_student=True
+
+print(type(name))
+
+
+gpa =int(gpa)
+print(gpa)
+
+age=float(age)
+print(age)
+
+age=str(age)
+print(age)
+print(type(age))
+age+="1"
+print(age)
+name=bool(name)
+print(name)
