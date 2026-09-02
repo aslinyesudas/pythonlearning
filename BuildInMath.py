5029dce80004af4aa9aaeeba0d3c0c1d375297a8
Aslin Yeasudas <aslinyesudas@gmail.com>
2026-09-02 23:57:25 +0530

    Built in math in python

---
+++ BuildInMath.py
@@ -0,0 +1,25 @@
+import math
+x=3.14
+y=-4
+z=5
+
+result = round(x)
+results = abs(y)
+resultss = pow(4,3)
+maximum=max(x,y,z)
+minimum=min(x,y,z)
+
+
+print(result)
+print(results)
+print(resultss)
+print(maximum)
+print(minimum)
+print(math.pi)
+print(math.e)
+res=math.sqrt(x)
+ress=math.ceil(x)
+print(res)
+print(ress)
+resss=math.floor(x)
+print(resss)
