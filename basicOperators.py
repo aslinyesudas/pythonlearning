bdd17f6b679bf4cf179fabe3edee245d5944f224
Aslin Yeasudas <aslinyesudas@gmail.com>
2026-09-02 23:50:30 +0530

    Basic operators in python

---
+++ basicOperators.py
@@ -0,0 +1,12 @@
+friends = 10
+friends = friends + 1
+friends+=friends
+friends-=2
+friends=friends*3
+friends=friends/2
+friends/=2
+friends=friends**2
+friends**=2
+remainder=friends % 3
+
+print(remainder)
