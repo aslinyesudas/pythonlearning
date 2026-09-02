bd3460f33c0cddba363e42025e445fa844201c3e
Aslin Yeasudas <aslinyesudas@gmail.com>
2026-09-02 23:29:23 +0530

    Exercise #1 - area of a rectangle

---
+++ RectangleArea.py
@@ -0,0 +1,6 @@
+# Excercise 1 Rectangle Area Cal
+
+length = float(input("Enter the length:"))
+width = float(input("Enter the width:"))
+area = length * width
+print(f"The area of the rectangle is : {area}")
