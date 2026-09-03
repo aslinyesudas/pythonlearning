776cf217194a870942fb124e9df80c48ee558b55
Aslin Yeasudas <aslinyesudas@gmail.com>
2026-09-03 23:17:51 +0530

    if else statements in python

--- if_else.py
+++ if_else.py
@@ -2,8 +2,9 @@
 #     Else do somthing else
 
 age=int(input("Enter your age:"))
-
-if age >= 18:
+if age >= 100:
+    print("You are too old to sign up")
+elif age >= 18:
     print("You are now signed up!")
 elif age <0:
     print("You haven't been born yet!")
