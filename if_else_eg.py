53393e1273f552421b0477d7064a8018936f83aa
Aslin Yeasudas <aslinyesudas@gmail.com>
2026-09-03 23:27:15 +0530

    if else eg

---
+++ if_else_eg.py
@@ -0,0 +1,14 @@
+for_sale=True
+
+if for_sale:
+    print("This item is for sale")
+else:
+    print("This item is not for sale")
+
+online=False
+
+if online:
+    print("The user is online")
+
+else:
+    print("The user is offline")
