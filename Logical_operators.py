fc51eff801cf92c58d4b1c62fdce57731e340ae1
Aslin Yeasudas <aslinyesudas@gmail.com>
2026-09-05 23:09:43 +0530

    logical operators

--- Logical_operators.py
+++ Logical_operators.py
@@ -11,3 +11,16 @@ if Temp>35 or Temp<0 or is_raining:
 
 else:
     print("The outdoor event is still scheduled")
+
+
+temp=25
+is_sunny=True
+if temp>=28 and is_sunny:
+    print("it is hot outside 🥵")
+    print("it is sunny 🌞")
+elif temp<=0 and is_sunny:
+    print("it is cold outside")
+    print("IT IS SUNNY")
+elif 28>temp>0 and is_sunny:
+    print("it is warm outside")
+    print("it is sunny")
