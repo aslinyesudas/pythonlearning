e81061b5eec6af190ffdced9687192f93bdb4a6e
Aslin Yeasudas <aslinyesudas@gmail.com>
2026-09-06 13:38:47 +0530

    valid user input exercise

---
+++ Validate_user_input.py
@@ -0,0 +1,20 @@
+# Validate user input exercise
+# 1. username is no more than 12 characters
+# 2. username must not contain spaces
+# 3. username must not contain digits
+
+username=input("Enter a username: ")
+
+
+
+if len(username) > 12:
+    print("Yiur username can't be more than 12 characters")
+
+elif not username.find(" ")==-1:
+    print("Your username can't contain spaces")
+
+elif not username.isalpha():
+    print("Your name can't contain numbres")
+
+else:
+    print(f"Welcome {username}")
