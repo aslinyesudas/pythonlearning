54834464842506971d29ece4de40d494807cb92c
Aslin Yeasudas <aslinyesudas@gmail.com>
2026-09-06 13:31:21 +0530

    string functions

---
+++ string_practice1.py
@@ -0,0 +1,15 @@
+
+name=input("Enter your full name : ")
+phone_number=input("Enter your phone number: ")
+#result=len(name)
+#result = name.find("o")
+#result=name.rfind("q")
+#result=name.capitalize()
+#result=name.upper()
+#result=name.lower()
+#result=name.isdigit()
+#result=name.isalpha()
+#result=phone_number.count("-")
+result=phone_number.replace("-"," ")
+
+print(result)
