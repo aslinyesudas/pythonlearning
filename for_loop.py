6cb1536b45918d2544a6c06d77a516d70c511f15
Aslin Yeasudas <aslinyesudas@gmail.com>
2026-09-07 23:01:55 +0530

    for loop

--- for_loop.py
+++ for_loop.py
@@ -1,7 +1,10 @@
 #for loop = execute a block of code a fixed number of times.
 #           You can iterate over a range, string, sequence, etc.
 
-for x in (range(1, 11, 2)):
-    print(x)
+for x in range(1, 21):
+    if x == 13:
+        break
+    else:
+        print(x)
 
 
