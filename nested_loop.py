6e8c1ac7c8d9decb44efacba044c384fa9f7c1a2
Aslin Yeasudas <aslinyesudas@gmail.com>
2026-09-07 23:15:14 +0530

    nested loop

---
+++ nested_loop.py
@@ -0,0 +1,10 @@
+# nested loop = A loop within another (outer, inner)
+#               outer loop:
+#                   inner loop:
+for x in range(3):
+      
+    for x in range(1, 10):
+        print(x, end="")
+    print()
+
+
