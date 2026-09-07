a3651461da35736dab93dfd5f262056a97fba333
Aslin Yeasudas <aslinyesudas@gmail.com>
2026-09-07 23:19:04 +0530

    nested loops

--- nested_loop.py
+++ nested_loop.py
@@ -1,10 +1,14 @@
 # nested loop = A loop within another (outer, inner)
 #               outer loop:
 #                   inner loop:
-for x in range(3):
-      
-    for x in range(1, 10):
-        print(x, end="")
+
+rows=int(input("Enter the # of rows: "))
+columns=int(input("Enter the # of columns: "))
+symbol = input("Enter a symbol to use: ")
+
+for x in range(rows): 
+    for x in range(columns):
+        print(symbol, end="")
     print()
 
 
