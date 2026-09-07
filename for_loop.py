aedf3acfeacc8ed2e96d4a65029406ec797873fb
Aslin Yeasudas <aslinyesudas@gmail.com>
2026-09-07 22:58:43 +0530

    for loop in python

--- for_loop.py
+++ for_loop.py
@@ -1,5 +1,7 @@
 #for loop = execute a block of code a fixed number of times.
 #           You can iterate over a range, string, sequence, etc.
 
-for x in range(1, 11):
+for x in reversed(range(1, 11)):
     print(x)
+
+print("happy new year!")
