7b7994629925ba1792fb33d62c6653b31c4f90e6
Aslin Yeasudas <aslinyesudas@gmail.com>
2026-09-08 20:09:14 +0530

    2D lists

---
+++ 2D_lists.py
@@ -0,0 +1,9 @@
+
+fruits = ["apple", "orange", "banana", "coconut"]
+vegetables = ["celery", "carrots", "potatoes"]
+meats = ["chicken", "fish", "tukey"]
+
+groceries = [fruits, vegetables, meats]
+
+
+print(groceries[0])
