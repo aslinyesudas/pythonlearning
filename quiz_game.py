92841c48178582ba11945827403f8db30cfee609
Aslin Yeasudas <aslinyesudas@gmail.com>
2026-09-08 23:08:56 +0530

    python guiz game

--- quiz_game.py
+++ quiz_game.py
@@ -34,3 +34,21 @@ for question in questions:
         print("INCORRECT!")
         print(f"{answers[question_num]} is the correct answer")
     question_num+=1
+
+print("---------------------")
+print("       RESULTS       ")
+print("---------------------")
+
+print("answers: ", end=" ")
+for answer in answers:
+    print(answer, end=" ")
+print()
+
+print("guesses: ", end=" ")
+for guess in guesses:
+    print(guess, end=" ")
+print()
+
+
+score = int(score / len(questions) * 100)
+print(f"Your score is: {score}%")
