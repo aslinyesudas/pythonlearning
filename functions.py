036acb8f4d25be391a7da352af03191d41c70d24
Aslin Yeasudas <aslinyesudas@gmail.com>
2026-09-11 23:53:06 +0530

    functions in python

---
+++ functions.py
@@ -0,0 +1,12 @@
+#functions = A block of reusable code
+#            place () after the function name to invoke it
+
+def happy_birthday(name, age):
+    print(f"Happy birthday to {name}!")
+    print(f"You are {age} years old!")
+    print("Happy birthday to you!")
+    print()
+
+happy_birthday("Bro", 20)
+happy_birthday("Steve", 30)
+happy_birthday("Joe", 40)
