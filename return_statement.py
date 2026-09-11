853dab729dfa38a316499dc7bc30d88da73f9784
Aslin Yeasudas <aslinyesudas@gmail.com>
2026-09-11 23:59:56 +0530

    return statement

---
+++ return_statement.py
@@ -0,0 +1,18 @@
+#return = statement used to end a function
+#         and send a result back to the caller
+
+def add (x, y):
+    z = x+y
+    return z
+
+def subtract(x, y):
+    z = x-y
+    return z
+
+def multiply(x, y):
+    z = x*y
+    return z
+
+def divide(x, y):
+    z = x/y
+    return z
