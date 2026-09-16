c0581a419013b1533b007d15867f12f24f67cc2b
Aslin Yeasudas <aslinyesudas@gmail.com>
2026-09-17 00:09:17 +0530

    list comprehension

---
+++ list_comprehension.py
@@ -0,0 +1,3 @@
+# List comprehension = A concise way to create lists in python
+#                      Compact and easier to read than traditional loops
+#                      [expression for value in iterable if condition]
