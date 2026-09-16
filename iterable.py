82c4d3ef68d68819f3447f3de5096f1eec96abe8
Aslin Yeasudas <aslinyesudas@gmail.com>
2026-09-16 23:51:53 +0530

    iterables in python

---
+++ iterable.py
@@ -0,0 +1,23 @@
+# Iterables = An object/collections that can return its elements one at a time,
+#             allowing it to be iterated over in a loop
+
+numbers = [1, 2, 3, 4, 5]
+
+for number in reversed(numbers):
+    print(number, end="-")
+
+fruits = ("apple", "orange", "banana", "coconut")
+
+for fruit in fruits:
+    print(fruit, end=" ")
+
+name = "Aslin"
+for character in name:
+    print()
+    print(character)
+
+my_dictionary = {"A":1, "B":2, "C":3}
+
+for key, value in my_dictionary.items():
+    print(f"{key} = {value}")
+    
