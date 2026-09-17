789e6fc7882de0038f4319dd8607b7da7bc509eb
Aslin Yeasudas <aslinyesudas@gmail.com>
2026-09-17 18:55:01 +0530

    list comprehension

--- list_comprehension.py
+++ list_comprehension.py
@@ -21,3 +21,7 @@ print(positive_nums)
 print(negative_nums)
 print(even_nums)
 print(odd_nums)
+
+grades = [85, 42, 79, 90, 56, 61, 30]
+passing_grades = [grade for grade in grades if grade >= 60]
+print(passing_grades)
