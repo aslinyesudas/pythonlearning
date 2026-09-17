2816991b0246c94f2392c98a7aa9114c34b8fde1
Aslin Yeasudas <aslinyesudas@gmail.com>
2026-09-17 18:52:57 +0530

    list comprehension

--- list_comprehension.py
+++ list_comprehension.py
@@ -9,5 +9,15 @@ squares = [z*2 for z in range(1, 11)]
 print(squares)
 
 fruits = ["apple", "orange", "banana", "coconut"]
-fruits = [fruit.upper() for fruit in fruits]
+fruit_chars = [fruit.upper() for fruit in fruits]
 print(fruits)
+
+numbers = [1, -2, 3, -4, 5, -6, 8, -7]
+positive_nums = [num for num in numbers if num >= 0]
+negative_nums = [num for num in numbers if num < 0]
+even_nums = [num for num in numbers if num % 2 == 0]
+odd_nums = [num for num in numbers if num % 2 == 1]
+print(positive_nums)
+print(negative_nums)
+print(even_nums)
+print(odd_nums)
