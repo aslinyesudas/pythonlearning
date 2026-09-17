b79e74418f791808e973c473fedb330f89884017
Aslin Yeasudas <aslinyesudas@gmail.com>
2026-09-17 19:13:21 +0530

    modules

---
+++ modules.py
@@ -0,0 +1,15 @@
+# Module = a file containing code you want to include in ypur program
+#          use 'import' to include a module (built-in or your own)
+#          useful to break up alarge program reusable separate files
+
+#import math
+#import math as m
+from math import e
+
+a, b, c, d =1, 2, 3, 4
+
+print(e ** a)
+print(e ** b)
+print(e ** c)
+print(e ** d)
+print(e ** e)
