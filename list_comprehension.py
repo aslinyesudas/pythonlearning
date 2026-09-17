e8a11197c0690478dbb2fc2a2447309ed57eb053
Aslin Yeasudas <aslinyesudas@gmail.com>
2026-09-17 18:47:34 +0530

    list comprehension

--- list_comprehension.py
+++ list_comprehension.py
@@ -3,5 +3,11 @@
 #                      [expression for value in iterable if condition]
 
 doubles = [x*2 for x in range(1, 11)]
+triples = [y*3 for y in range(1, 11)]
+squares = [z*2 for z in range(1, 11)]
 
-print(doubles)
+print(squares)
+
+fruits = ["apple", "orange", "banana", "coconut"]
+fruits = [fruit.upper() for fruit in fruits]
+print(fruits)
