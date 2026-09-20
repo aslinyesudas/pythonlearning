06000d94c81fc30ed19c9fd68392eaa4f24d71d4
Aslin Yeasudas <aslinyesudas@gmail.com>
2026-09-21 00:39:27 +0530

    encrption program in python

--- printing_encrypted_msg.py
+++ printing_encrypted_msg.py
@@ -1,7 +1,5 @@
 import random
 import string
-import enum
-
 
 chars = " " + string.punctuation + string.digits + string.ascii_letters
 chars = list(chars)
@@ -12,7 +10,7 @@ random.shuffle(key)
 print(f"chars: {chars}")
 print(f"key: {key}")
 
-#ENCRYPT
+# ENCRYPT
 plain_text = input("Enter a message to encrypt: ")
 cipher_text = ""
 
@@ -20,5 +18,16 @@ for letter in plain_text:
     index = chars.index(letter)
     cipher_text += key[index]
 
-print(f"original message: {plain_text}")
-print(f"encypted message: {cipher_text}")
+print(f"Original message: {plain_text}")
+print(f"Encrypted message: {cipher_text}")
+
+# DECRYPT
+cipher_text = input("Enter a message to decrypt: ")
+plain_text = ""
+
+for letter in cipher_text:
+    index = key.index(letter)
+    plain_text += chars[index]
+
+print(f"Encrypted message: {cipher_text}")
+print(f"Original message: {plain_text}")
