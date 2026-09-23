2f2282d246a7fbfb84df09ad0b54e4f613ca53da
Aslin Yeasudas <aslinyesudas@gmail.com>
2026-09-23 23:01:50 +0530

    class variable

--- class_variables.py
+++ class_variables.py
@@ -20,6 +20,6 @@ student4 = Student("Sandy", 27)
 print(f"My graduating class of {Student.class_year} has {Student.num_students} students")
 
 print(student1.name)
-print(student2.age)
+print(student1.age)
 print(Student.class_year)
 print(Student.num_students)
