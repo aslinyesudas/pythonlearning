b5b64d3b613adfc6631ef0f3af284f8fefb0f140
Aslin Yeasudas <aslinyesudas@gmail.com>
2026-09-26 23:35:23 +0530

    polymorphism

---
+++ polymorphism.py
@@ -0,0 +1,7 @@
+# polymorphism = Greek word that means to "have many forms or faces"
+#                Poly = Many
+#                Morphe = Form
+
+#                TWO WAYS TO ACHIEVE POLYMORPHISM
+#                1. Inheritance = An object could be treated of the same type as a parent class
+#                2. "Duck typing" = Object must have necessary attribute/methods
